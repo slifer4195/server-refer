3b633389a4817f19511edae4192e5d2a31fb776c
Sung Rung Yoo <slifer4195@gmail.com>
2025-10-11 18:47:38 -0600

    db url fix

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ api_key = os.environ.get("API_KEY")
 
 class Config:
     SECRET_KEY = api_key
-    SQLALCHEMY_DATABASE_URI = os.environ.get("postgresql://postgres:VXXrjJwXbhbiPtdguciLWXXYcgcZVeGT@switchback.proxy.rlwy.net:49627/railway")  # <-- use Railway DB
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  # <-- use Railway DB
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SESSION_TYPE = 'filesystem'
--- app/test/apiKey.py
+++ app/test/apiKey.py
@@ -1,2 +1,5 @@
 import secrets
+import os
+
 print(secrets.token_hex(32))
+print("DB URL:", os.environ.get("DATABASE_URL"))
