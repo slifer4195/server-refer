7afdb36348afe5b412891b90d8f4549087fd503c
Sung Rung Yoo <slifer4195@gmail.com>
2025-10-19 16:09:01 -0600

    cookie

--- app/config.py
+++ app/config.py
@@ -5,11 +5,11 @@ load_dotenv()
 api_key = os.environ.get("API_KEY")
 
 class Config:
-    SECRET_KEY = api_key
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")  # <-- use Railway DB
+    SECRET_KEY = api_key 
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SESSION_TYPE = 'filesystem'
     SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SECURE = False
-    SESSION_COOKIE_SAMESITE = 'Lax'
+    SESSION_COOKIE_SECURE = True        # must be True for HTTPS
+    SESSION_COOKIE_SAMESITE = 'None'    # allow cross-site requests
